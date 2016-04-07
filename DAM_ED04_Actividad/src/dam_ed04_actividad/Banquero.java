01dc2f98b70d6335bce64d7e3528a73be1578011
1-DAM-T <1-DAM-T>
2016-04-07 18:36:11 +0100

    commit 6

--- DAM_ED04_Actividad/src/dam_ed04_actividad/Banquero.java
+++ DAM_ED04_Actividad/src/dam_ed04_actividad/Banquero.java
@@ -10,5 +10,25 @@ package dam_ed04_actividad;
  * @author 1-DAM-T
  */
 public class Banquero {
-    
+
+    public Banquero() {
+    }
+    private String nombre;
+    private String dni;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }
